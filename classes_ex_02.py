3973b2ebdf9240b31befb06edabfc624bde5aaa7
AlexandreJefersonPereiradaSilva <dairodrigues281@gmail.com>
2025-09-09 15:50:51 -0300

    Curso Python

---
+++ classes_ex_02.py
@@ -0,0 +1,7 @@
+class Animal_Marinho():
+    def __init__(self, nome, nome_cientifico, cor, sexo, alimentação):
+        self.nome = nome
+        self.nome_cientifico = nome_cientifico
+        self.cor = cor
+        self.sexo = sexo
+        self.alimentação = alimentação
